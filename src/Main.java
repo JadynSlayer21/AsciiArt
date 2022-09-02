85f499e1b1ae5a1e0435ff7f47b1507786b36540
100182426 <JadynSlayer21>
2022-09-02 08:50:39 -0500

    Frog and Pig made Bird made

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
         System.out.println("              ");
         System.out.println("  ___");
         System.out.println("(' V ')");
+        System.out.println("((___))");
+        System.out.println("  ^ ^ ");
 
 
     }
